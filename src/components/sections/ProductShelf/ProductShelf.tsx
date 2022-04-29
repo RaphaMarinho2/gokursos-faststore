28da15415cbf6857001ca314ac8c099fe69c2f14
Lucas Rodrigues <lucas.rodrigues@acct.global>
2022-04-29 11:47:02 -0300

    start faststore base

---
+++ src/components/sections/ProductShelf/ProductShelf.tsx
@@ -0,0 +1,58 @@
+import ProductShelfSkeleton from 'src/components/skeletons/ProductShelfSkeleton'
+import { useProductsQuery } from 'src/sdk/product/useProductsQuery'
+import type { ProductsQueryQueryVariables } from '@generated/graphql'
+
+import ProductCard from '../../product/ProductCard'
+import Section from '../Section'
+
+interface ProductShelfProps extends Partial<ProductsQueryQueryVariables> {
+  title: string | JSX.Element
+  withDivisor?: boolean
+}
+
+function ClkBtn() {
+  console.info('click')
+}
+
+function ProductBtnBuyInPage() {
+  return <button onClick={ClkBtn}>Click me</button>
+}
+
+function ProductShelf({
+  title,
+  withDivisor = false,
+  ...variables
+}: ProductShelfProps) {
+  console.info('ProductShelf')
+
+  const products = useProductsQuery(variables)
+
+  if (products?.edges.length === 0) {
+    return null
+  }
+
+  return (
+    <Section
+      className={`layout__section ${withDivisor ? 'section__divisor' : ''}`}
+    >
+      <h2 className="text__title-section layout__content">{title}</h2>
+      <div data-fs-product-shelf>
+        <ProductShelfSkeleton loading={products === undefined}>
+          <ul data-fs-product-shelf-items className="layout__content">
+            {products?.edges.map((product, idx) => (
+              <li key={`${product.node.id}`}>
+                <ProductCard
+                  product={product.node}
+                  index={idx + 1}
+                  ButtonBuy={ProductBtnBuyInPage()}
+                />
+              </li>
+            ))}
+          </ul>
+        </ProductShelfSkeleton>
+      </div>
+    </Section>
+  )
+}
+
+export default ProductShelf
