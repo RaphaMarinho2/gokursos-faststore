36c56012ee6f78c158c940948e5bff512c71f650
Lipe <felipe.bittencourt@acct.global>
2022-05-23 13:40:23 -0300

    PDP

--- src/components/ui/ProductTitle/ProductTitle.tsx
+++ src/components/ui/ProductTitle/ProductTitle.tsx
@@ -13,22 +13,16 @@ interface ProductTitleProp {
   /**
    * A text to be used below the title and the label, such as the product's reference number.
    */
-  refNumber?: string
+  // refNumber?: string
 }
 
-function ProductTitle({ title, label, refNumber }: ProductTitleProp) {
+function ProductTitle({ title, label }: ProductTitleProp) {
   return (
     <div className="product-title">
       <div className="product-title__header">
         {title}
         {!!label && label}
       </div>
-
-      {refNumber && (
-        <div className="product-title__addendum / text__legend">
-          Ref.: {refNumber}
-        </div>
-      )}
     </div>
   )
 }
