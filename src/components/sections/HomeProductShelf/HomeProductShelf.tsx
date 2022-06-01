0de8ebe14a5e2a93756ddd47ce80db4fa84fc87a
Raphael Marinho <raphael.marinho+github@acct.global>
2022-06-01 15:03:10 -0300

    fix: fixed card quantity on home shelf

---
+++ src/components/sections/HomeProductShelf/HomeProductShelf.tsx
@@ -0,0 +1,27 @@
+import type { ProductDetailsFragment_ProductFragment } from '@generated/graphql'
+import ProductShelf from 'src/components/product/ProductShelf'
+import useWindowDimensions from 'src/sdk/utils/useWindowDimensions'
+import { ITEMS_PER_SECTION } from 'src/constants'
+
+import Section from '../Section'
+
+import './home-product-shelf.scss'
+
+const HomeProductShelf = () => {
+  const { isMobile } = useWindowDimensions()
+
+  const shelfItemQuantity = isMobile ? 2 : 5
+
+  return (
+    <Section className="layout__content home-shelf-container">
+      <ProductShelf
+        cardsQuantity={shelfItemQuantity}
+        first={ITEMS_PER_SECTION}
+        title="Cursos relacionados"
+        withDivisor
+      />
+    </Section>
+  )
+}
+
+export default HomeProductShelf
