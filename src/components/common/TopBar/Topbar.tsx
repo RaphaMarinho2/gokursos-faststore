635ddfab4ed9be499b0edea079a4a02e6ae808b4
Natasha Freitas <natasha.freitas@acct.global>
2022-06-03 11:34:47 -0300

    fix: add hover effect on category items

--- src/components/common/TopBar/Topbar.tsx
+++ src/components/common/TopBar/Topbar.tsx
@@ -21,90 +21,92 @@ const Topbar = () => {
 
   return (
     <div className="topbar-wrapper">
-      <LeiaJaLogo />
-      <div className="topbar-categories-wrapper">
-        <div className="topbar-category-divider" />
-        <Link
-          style={{ borderBottom: '2px solid #E0272E' }}
-          className="topbar-category-item"
-          href="https://www.leiaja.com/noticias/?utm_source=gokursos"
-          target="_blank"
-        >
-          NOTÍCIA
-        </Link>
-        <div className="topbar-category-divider" />
-        <Link
-          style={{ borderBottom: '2px solid #234D7B' }}
-          className="topbar-category-item"
-          href="https://www.leiaja.com/politica/?utm_source=gokursos"
-          target="_blank"
-        >
-          POLÍTICA
-        </Link>
-        <div className="topbar-category-divider" />
-        <Link
-          style={{ borderBottom: '2px solid #000000' }}
-          className="topbar-category-item"
-          href="
+      <div className="topbar-content-wrapper">
+        <LeiaJaLogo />
+        <div className="topbar-categories-wrapper">
+          <div className="topbar-category-divider" />
+          <Link
+            style={{ borderBottom: '2px solid #E0272E' }}
+            className="topbar-category-item"
+            href="https://www.leiaja.com/noticias/?utm_source=gokursos"
+            target="_blank"
+          >
+            NOTÍCIA
+          </Link>
+          <div className="topbar-category-divider" />
+          <Link
+            style={{ borderBottom: '2px solid #234D7B' }}
+            className="topbar-category-item"
+            href="https://www.leiaja.com/politica/?utm_source=gokursos"
+            target="_blank"
+          >
+            POLÍTICA
+          </Link>
+          <div className="topbar-category-divider" />
+          <Link
+            style={{ borderBottom: '2px solid #000000' }}
+            className="topbar-category-item"
+            href="
           https://www.leiaja.com/carreiras/?utm_source=gokursos"
-          target="_blank"
-        >
-          CARREIRAS
-        </Link>
-        <div className="topbar-category-divider" />
-        <Link
-          style={{ borderBottom: '2px solid #176507' }}
-          className="topbar-category-item"
-          href="
+            target="_blank"
+          >
+            CARREIRAS
+          </Link>
+          <div className="topbar-category-divider" />
+          <Link
+            style={{ borderBottom: '2px solid #176507' }}
+            className="topbar-category-item"
+            href="
 
           https://www.leiaja.com/esportes/?utm_source=gokursos"
-          target="_blank"
-        >
-          ESPORTES
-        </Link>
-        <div className="topbar-category-divider" />
-        <Link
-          style={{ borderBottom: '2px solid #DF8129' }}
-          className="topbar-category-item"
-          href="https://www.leiaja.com/cultura/?utm_source=gokursos"
-          target="_blank"
-        >
-          ENTRETENIMENTO
-        </Link>
-        <div className="topbar-category-divider" />
-        <Link
-          style={{ borderBottom: '2px solid #4EB2EB' }}
-          className="topbar-category-item"
-          href="https://www.leiaja.com/tecnologia/?utm_source=gokursos"
-          target="_blank"
-        >
-          TECNOLOGIA
-        </Link>
-        <div className="topbar-category-divider" />
+            target="_blank"
+          >
+            ESPORTES
+          </Link>
+          <div className="topbar-category-divider" />
+          <Link
+            style={{ borderBottom: '2px solid #DF8129' }}
+            className="topbar-category-item"
+            href="https://www.leiaja.com/cultura/?utm_source=gokursos"
+            target="_blank"
+          >
+            ENTRETENIMENTO
+          </Link>
+          <div className="topbar-category-divider" />
+          <Link
+            style={{ borderBottom: '2px solid #4EB2EB' }}
+            className="topbar-category-item"
+            href="https://www.leiaja.com/tecnologia/?utm_source=gokursos"
+            target="_blank"
+          >
+            TECNOLOGIA
+          </Link>
+          <div className="topbar-category-divider" />
+        </div>
+        <form className="topbar-search" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            ref={searchInputRef}
+            placeholder=""
+            className="topbar-search"
+            onChange={handleInputValue}
+          />
+          <Link
+            href={
+              inputValue !== ''
+                ? `https://www.leiaja.com/search/site/${encodeURIComponent(
+                    inputValue
+                  )}`
+                : 'https://www.leiaja.com/search/site'
+            }
+            id="submit-button"
+            className="submit-button"
+            target="_blank"
+          >
+            <SearchIcon />
+          </Link>
+        </form>
       </div>
-      <form className="topbar-search" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          ref={searchInputRef}
-          placeholder=""
-          className="topbar-search"
-          onChange={handleInputValue}
-        />
-        <Link
-          href={
-            inputValue !== ''
-              ? `https://www.leiaja.com/search/site/${encodeURIComponent(
-                  inputValue
-                )}`
-              : 'https://www.leiaja.com/search/site'
-          }
-          id="submit-button"
-          className="submit-button"
-          target="_blank"
-        >
-          <SearchIcon />
-        </Link>
-      </form>
     </div>
   )
 }
