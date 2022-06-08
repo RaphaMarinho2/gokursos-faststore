22c6116312a769defa876bc20dc8e362cf8d5bb7
Natasha Freitas <natasha.freitas@acct.global>
2022-06-08 12:02:13 -0300

    console log

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,11 @@ function Page(props: Props) {
     location: { pathname, host },
   } = props
 
+  console.log('spaceId', process.env.GATSBY_CONTENTFUL_SPACE_ID)
+  console.log('token', process.env.GATSBY_CONTENTFUL_CDAPI_TOKEN)
+  console.info('spaceId', process.env.GATSBY_CONTENTFUL_SPACE_ID)
+  console.info('token', process.env.GATSBY_CONTENTFUL_CDAPI_TOKEN)
+
   const { locale } = useSession()
 
   const title = site?.siteMetadata?.title ?? ''
