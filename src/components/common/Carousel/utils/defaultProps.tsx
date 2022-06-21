8b8f4c20809f3b0e6d9874060f55f8653ae7df52
Willian Ottoni <willian.ottoni@acct.global>
2022-06-21 17:06:00 -0300

    feat: added carousel component

---
+++ src/components/common/Carousel/utils/defaultProps.tsx
@@ -0,0 +1,19 @@
+export const DefaultProps = {
+  styles: {
+    bullets: {
+      width: '15px',
+      height: '15px',
+      borderStyle: 'none',
+      borderRadius: '30px',
+      margin: '7px',
+    },
+    arrows: {
+      width: '40px',
+      height: ' 40px',
+      color: 'white',
+      borderStyle: 'none',
+      borderRadius: '30px',
+      margin: '10px',
+    },
+  },
+}
