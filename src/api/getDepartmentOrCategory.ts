95823684c5db73a5e186c51bad7b2b55909085f4
Natasha Freitas <natasha.freitas@acct.global>
2022-07-05 16:02:42 -0300

    feat(department): include dynamic creation of pages based on department/category catalog
    n

---
+++ src/api/getDepartmentOrCategory.ts
@@ -0,0 +1,24 @@
+import type { GatsbyFunctionRequest, GatsbyFunctionResponse } from 'gatsby'
+import axios from 'axios'
+
+export default async function getDepartmentOrCategory(
+  req: GatsbyFunctionRequest,
+  res: GatsbyFunctionResponse
+) {
+  const { departmentName, categoryName } = req.query
+
+  try {
+    const filterParam =
+      categoryName === 'false'
+        ? `Department/Slug eq '${departmentName}'`
+        : `Category/Slug eq '${categoryName}'`
+
+    const { data } = await axios.get(
+      `https://truechangepresales-test.mendixcloud.com/odata/Catalog/v1/Products?$expand=SKU, Department, Category, Price, Checkout, Especificacao, CommercialCondition, TradePolicy, Stock, Rank, Brand, Especificacao/CargaHoraria&$orderby=Rank/Score desc&$skip=0&$filter=${filterParam}&$top=20&$select=Name, ProductImageURL, Price/BasePrice, Price/ListPrice, Price/CommisionedPrice, Price/isSale, Department/Name, Category/Name, Especificacao/CargaHoraria/Text`
+    )
+
+    res.json(data)
+  } catch (error) {
+    throw new Error(error)
+  }
+}
--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -1,76 +1,47 @@
-import { parseSearchState, SearchProvider, useSession } from '@faststore/sdk'
-import { gql } from '@vtex/graphql-utils'
-import { graphql } from 'gatsby'
-import { BreadcrumbJsonLd, GatsbySeo } from 'gatsby-plugin-next-seo'
-import { useMemo } from 'react'
-import Breadcrumb from 'src/components/sections/Breadcrumb'
-import Hero from 'src/components/sections/Hero'
-import ProductGallery from 'src/components/sections/ProductGallery'
-import ProductShelf from 'src/components/product/ProductShelf'
+import ServerCollectionPageQuery from 'src/mocks/ServerCollectionPageQuery.json'
 import ScrollToTopButton from 'src/components/sections/ScrollToTopButton'
-import Icon from 'src/components/ui/Icon'
-import { ITEMS_PER_PAGE, ITEMS_PER_SECTION } from 'src/constants'
-import { applySearchState } from 'src/sdk/search/state'
+import ProductGallery from 'src/components/sections/ProductGallery'
+import Breadcrumb from 'src/components/sections/Breadcrumb'
 import { mark } from 'src/sdk/tests/mark'
-import type {
-  CollectionPageQueryQuery,
-  ServerCollectionPageQueryQuery,
-  CollectionPageQueryQueryVariables,
-} from '@generated/graphql'
+import { SearchProvider, parseSearchState } from '@faststore/sdk'
+import { applySearchState } from 'src/sdk/search/state'
+import { ITEMS_PER_PAGE } from 'src/constants'
+import BannerCategory from 'src/components/sections/BannerCategory'
 import type { PageProps } from 'gatsby'
-import type { SearchState } from '@faststore/sdk'
+import { graphql } from 'gatsby'
+import axios from 'axios'
+import type { DepartmentPageQueryQuery } from '@generated/graphql'
 
-import 'src/styles/pages/plp.scss'
-
-type Props = PageProps<
-  CollectionPageQueryQuery,
-  CollectionPageQueryQueryVariables,
-  unknown,
-  ServerCollectionPageQueryQuery
-> & { slug: string }
-
-const useSearchParams = (props: Props): SearchState => {
-  const {
-    location: { href, pathname },
-    serverData: { collection },
-  } = props
-
-  const selectedFacets = collection?.meta.selectedFacets
-
-  return useMemo(() => {
-    const maybeState = href ? parseSearchState(new URL(href)) : null
-
-    return {
-      page: maybeState?.page ?? 0,
-      base: maybeState?.base ?? pathname,
-      selectedFacets:
-        maybeState && maybeState.selectedFacets.length > 0
-          ? maybeState.selectedFacets
-          : selectedFacets ?? [],
-      term: maybeState?.term ?? null,
-      sort: maybeState?.sort ?? 'score_desc',
-    }
-  }, [href, pathname, selectedFacets])
-}
-
+export type Props = PageProps<DepartmentPageQueryQuery>
 function Page(props: Props) {
   const {
-    data: { site },
-    serverData: { collection },
-    location: { host },
-    slug,
+    data: { allContentfulBannerDepartmentCategory },
   } = props
 
-  const { locale } = useSession()
-  const searchParams = useSearchParams(props)
+  // const {
+  //   data: { site },
+  //   serverData: { collection },
+  //   location: { host },
+  //   slug,
+  // } = props
 
-  const { page } = searchParams
-  const title = collection?.seo.title ?? site?.siteMetadata?.title ?? ''
-  const pageQuery = page !== 0 ? `?page=${page}` : ''
-  const canonical =
-    host !== undefined
-      ? `https://${host}/${slug}/${pageQuery}`
-      : `/${slug}/${pageQuery}`
+  // console.log('props', props)
+
+  const collection = ServerCollectionPageQuery
+
+  const title = collection?.seo.title ?? ''
+
+  const maybeState = parseSearchState(
+    new URL('https://deploy-preview-26--gokursos.netlify.app/tecnologia/')
+  )
+
+  const searchParams = {
+    page: maybeState?.page,
+    base: maybeState?.base,
+    selectedFacets: maybeState ? maybeState.selectedFacets : [],
+    term: maybeState?.term ?? null,
+    sort: maybeState?.sort ?? 'score_desc',
+  }
 
   return (
     <SearchProvider
@@ -78,57 +49,14 @@ function Page(props: Props) {
       itemsPerPage={ITEMS_PER_PAGE}
       {...searchParams}
     >
-      {/* SEO */}
-      <GatsbySeo
-        title={title}
-        titleTemplate={site?.siteMetadata?.titleTemplate ?? ''}
-        description={site?.siteMetadata?.description ?? ''}
-        canonical={canonical}
-        language={locale}
-        openGraph={{
-          type: 'website',
-          title,
-          description: site?.siteMetadata?.description ?? '',
-        }}
-      />
-      <BreadcrumbJsonLd
-        itemListElements={collection?.breadcrumbList.itemListElement ?? []}
-      />
-
-      {/*
-        WARNING: Do not import or render components from any
-        other folder than '../components/sections' in here.
-
-        This is necessary to keep the integration with the CMS
-        easy and consistent, enabling the change and reorder
-        of elements on this page.
-
-        If needed, wrap your component in a <Section /> component
-        (not the HTML tag) before rendering it here.
-      */}
       <Breadcrumb
         breadcrumbList={collection?.breadcrumbList.itemListElement}
         name={title}
       />
 
-      <Hero
-        variant="secondary"
-        title={title}
-        subtitle={`All the amazing ${title} from the brands we partner with.`}
-        imageSrc="https://storeframework.vtexassets.com/arquivos/ids/190897/Photo.jpg"
-        imageAlt="Quest 2 Controller on a table"
-        icon={<Icon name="Headphones" width={48} height={48} weight="thin" />}
-      />
+      <BannerCategory nodes={allContentfulBannerDepartmentCategory.nodes} />
 
       <ProductGallery title={title} />
-
-      <ProductShelf
-        first={ITEMS_PER_SECTION}
-        sort="score_desc"
-        title="You might also like"
-        pretitle=""
-        withDivisor
-      />
 
       <ScrollToTopButton />
     </SearchProvider>
@@ -136,54 +64,54 @@ function Page(props: Props) {
 }
 
 export const querySSG = graphql`
-  query CollectionPageQuery {
+  query DepartmentPageQuery {
     site {
       siteMetadata {
+        title
+        description
         titleTemplate
-        title
-        description
       }
     }
-  }
-`
-
-export const querySSR = gql`
-  query ServerCollectionPageQuery($slug: String!) {
-    collection(slug: $slug) {
-      seo {
+    allContentfulBannerDepartmentCategory {
+      nodes {
         title
-        description
-      }
-      breadcrumbList {
-        itemListElement {
-          item
-          name
-          position
+        subtitle
+        imageDesktop {
+          url
         }
-      }
-      meta {
-        selectedFacets {
-          key
-          value
+        imageMobile {
+          url
         }
       }
     }
   }
 `
 
-export const getServerData = async ({
-  params: { slug },
-}: {
-  params: Record<string, string>
-}) => {
+export const getServerData = async (props: Props) => {
+  const {
+    params: { slug },
+  } = props
+
+  const categoryName = slug.includes('/') && slug.split('/')[1]
+  const departmentName = !slug.includes('/') && slug
+
   try {
-    const { execute } = await import('src/server/index')
-    const { data } = await execute({
-      operationName: querySSR,
-      variables: { slug },
-    })
+    const data = await axios
+      .get('/api/getDepartmentOrCategory', {
+        proxy: {
+          protocol: 'http',
+          host: 'localhost',
+          port: 8000,
+        },
+        url: 'http://localhost:8000',
+        params: {
+          departmentName,
+          categoryName,
+        },
+      })
+      .then(({ data: { value } }) => value)
 
-    if (data === null) {
+    if (!data.length) {
       const originalUrl = `/${slug}`
 
       return {
