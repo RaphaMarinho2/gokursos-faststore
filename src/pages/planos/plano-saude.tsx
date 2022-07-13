cd80bf66d20134fedd908ee2ae0ebe9462f4ed17
Lucas Rodrigues <lucas.rodrigues+github@acct.global>
2022-07-13 13:43:05 -0300

    feat(plan-page teste component and fix productgalleryquery): product gallery test

--- src/pages/planos/plano-saude.tsx
+++ src/pages/planos/plano-saude.tsx
@@ -5,6 +5,7 @@ import { mark } from 'src/sdk/tests/mark'
 import SimpleText from 'src/components/sections/SimpleText/SimpleText'
 import Breadcrumb from 'src/components/sections/Breadcrumb'
 import Section from 'src/components/sections/Section'
+import MultiRangeSlider from 'src/components/sections/MulRangeSlider'
 
 type ItemListType = {
   item: string
@@ -33,6 +34,10 @@ function Page(props: Props) {
   ]
 
   const title = 'Conheça os planos GoKursos'
+  const priceByApi = {
+    min: 1.0,
+    max: 100.0,
+  }
 
   return (
     <Section>
@@ -40,6 +45,11 @@ function Page(props: Props) {
       <SimpleText
         textReceived={allContentfulPlanosTextoSimples}
         className="text-banner-bottom"
+      />
+      <MultiRangeSlider
+        min={priceByApi.min}
+        max={priceByApi.max}
+        onChange={({ min, max }) => console.info(`min = ${min}, max = ${max}`)}
       />
     </Section>
   )
