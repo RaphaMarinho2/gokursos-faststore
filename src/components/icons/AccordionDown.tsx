00c35417996577074879ea4e490dc36bf2fbc24e
Lucas Rodrigues <lucas.rodrigues+github@acct.global>
2022-07-13 13:34:52 -0300

    feat(icon accordion filter): icon accordion filter

---
+++ src/components/icons/AccordionDown.tsx
@@ -0,0 +1,21 @@
+import type { FC } from 'react'
+
+const AccordionDown: FC = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="18"
+    height="11"
+    viewBox="0 0 18 11"
+    fill="none"
+  >
+    <path
+      d="M1 1.37L9 9.37L17 1.36999"
+      stroke="#555555"
+      strokeWidth="1.75"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
+
+export default AccordionDown
