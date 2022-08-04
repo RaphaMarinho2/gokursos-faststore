ce6c77d437040cf926832e56cd441e54827fb3dd
Rodrigo Muniz <32344098+rcmuniz1994@users.noreply.github.com>
2022-08-04 09:08:27 -0300

    Feature/GM-100 (#104)
    * feat(productdetails): add padding on product details and weight on product name
    
    * feat(productdetails): change font color and weight of the prices
    
    * feat(buybutton): change cart icon
    
    * feat(instalments): add instalments on product details
    
    * feat(productdetails): add product tag to show the product category
    
    * feat(productdetails): change buy button font weight
    
    * feat(workload): add workload component on product details
    
    * feat(ratingsummary): add rating summary on product details
    
    * fix(productdetails): fix spacing
    
    n

---
+++ src/components/icons/Clock.tsx
@@ -0,0 +1,25 @@
+const ClockIcon = () => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle
+      cx="8.00002"
+      cy="7.99996"
+      r="6.33333"
+      stroke="#666666"
+      strokeWidth="2"
+    />
+    <path
+      d="M11 7.99996H8.25C8.11193 7.99996 8 7.88803 8 7.74996V5.66663"
+      stroke="#666666"
+      strokeWidth="2"
+      strokeLinecap="round"
+    />
+  </svg>
+)
+
+export default ClockIcon
