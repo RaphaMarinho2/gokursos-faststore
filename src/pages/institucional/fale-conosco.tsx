b69fc313399ab06f1c2c155298611ea1b795d988
Rodrigo Muniz <rodrigo.muniz@acct.global>
2022-08-05 10:44:07 -0300

    refactor(faleconosco): create a const for the breadcrumb

--- src/pages/institucional/fale-conosco.tsx
+++ src/pages/institucional/fale-conosco.tsx
@@ -1,24 +1,25 @@
 import { mark } from 'src/sdk/tests/mark'
-import Section from 'src/components/sections/Section'
 import InstitutionalMenu from 'src/components/sections/InstutionalMenu/InstitutionalMenu'
-import { BreadcrumbJsonLd } from 'gatsby-plugin-next-seo'
 import Breadcrumb from 'src/components/sections/Breadcrumb'
+import ContactUs from 'src/components/sections/ContactUs'
+import { BreadcrumbJsonLd } from 'gatsby-plugin-next-seo'
+
+const BREADCRUMB = [
+  {
+    item: '',
+    name: 'Fale Conosco',
+    position: 1,
+  },
+]
 
 function Page() {
-  const breadCrumb = [
-    {
-      item: '',
-      name: 'Fale Conosco',
-      position: 1,
-    },
-  ]
-
   return (
-    <Section>
-      <BreadcrumbJsonLd itemListElements={breadCrumb} />
-      <Breadcrumb breadcrumbList={breadCrumb} name="Fale Conosco" />
+    <>
+      <BreadcrumbJsonLd itemListElements={BREADCRUMB} />
+      <Breadcrumb breadcrumbList={BREADCRUMB} name="Fale Conosco" />
       <InstitutionalMenu />
-    </Section>
+      <ContactUs />
+    </>
   )
 }
 
