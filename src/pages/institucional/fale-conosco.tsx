d4138197f9c36cf5097fca4f2decc77c6c171cdf
Rodrigo Muniz <rodrigo.muniz@acct.global>
2022-08-09 08:54:28 -0300

    feat(fale-conosco): add location prop on institutional menu on the fale-conosco page

--- src/pages/institucional/fale-conosco.tsx
+++ src/pages/institucional/fale-conosco.tsx
@@ -1,8 +1,10 @@
+import type { PageProps } from 'gatsby'
+import { graphql } from 'gatsby'
 import { mark } from 'src/sdk/tests/mark'
 import InstitutionalMenu from 'src/components/sections/InstutionalMenu/InstitutionalMenu'
 import Breadcrumb from 'src/components/sections/Breadcrumb'
-import ContactUs from 'src/components/sections/ContactUs'
 import { BreadcrumbJsonLd } from 'gatsby-plugin-next-seo'
+import type { InstitutionalContactUsQuery } from '@generated/graphql'
 
 const BREADCRUMB = [
   {
@@ -12,16 +14,29 @@ const BREADCRUMB = [
   },
 ]
 
-function Page() {
+type Props = PageProps<InstitutionalContactUsQuery>
+
+function Page(props: Props) {
   return (
     <>
       <BreadcrumbJsonLd itemListElements={BREADCRUMB} />
       <Breadcrumb breadcrumbList={BREADCRUMB} name="Fale Conosco" />
-      <InstitutionalMenu />
-      <ContactUs />
+      <InstitutionalMenu location={props.location.pathname} />
     </>
   )
 }
+
+export const querySSG = graphql`
+  query InstitutionalContactUs {
+    site {
+      siteMetadata {
+        title
+        description
+        titleTemplate
+      }
+    }
+  }
+`
 
 Page.displayName = 'Page'
 export default mark(Page)
