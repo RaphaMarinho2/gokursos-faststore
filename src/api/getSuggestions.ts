bd3a8eac25663ec4990f4409a166c6124213c01f
Rodrigo Muniz <rodrigo.muniz@acct.global>
2022-08-12 16:58:45 -0300

    fix(getsuggestions): remove unneeded filters

--- src/api/getSuggestions.ts
+++ src/api/getSuggestions.ts
@@ -9,7 +9,7 @@ async function getSuggestions(
     const { term } = req.query
 
     const data = await axios.get(
-      `https://truechangepresales-test.mendixcloud.com/odata/Catalog/v1/Products?$expand=SKU, Department, Category, Price, Checkout, Especificacao, CommercialCondition, TradePolicy, Stock, Rank, Especificacao/CargaHoraria&$filter=IsActive eq true and (contains(Name, '${term}') or contains(Department/Name, '${term}') or contains(Category/Name, '${term}') or contains(KeyWords, '${term}') or contains(Description, '${term}') or contains(DescriptionShort, '${term}') or contains(Especificacao/Conteudo, '${term}') or contains(Especificacao/Objetivos, '${term}') or contains(Brand/Name, '${term}'))&$top=8&$skip=0&$select=Name, ProductImageURL, LinkId`
+      `https://truechangepresales-test.mendixcloud.com/odata/Catalog/v1/Products?$expand=SKU, Department, Category, Price, Checkout, Especificacao, CommercialCondition, TradePolicy, Stock, Rank, Especificacao/CargaHoraria&$filter=IsActive eq true and (contains(Name, '${term}') or contains(Department/Name, '${term}') or contains(Category/Name, '${term}') or contains(KeyWords, '${term}') or contains(Brand/Name, '${term}'))&$top=8&$skip=0&$select=Name, ProductImageURL, LinkId`
     )
 
     res.json(data.data?.value ?? [])
