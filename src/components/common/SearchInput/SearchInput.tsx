04c7a3475108445a6c8a84d0e733f1fa669419cc
Rodrigo Muniz <rodrigo.muniz@acct.global>
2022-08-12 10:49:51 -0300

    feat(searchinput): add deferred term to search suggestions

--- src/components/common/SearchInput/SearchInput.tsx
+++ src/components/common/SearchInput/SearchInput.tsx
@@ -7,6 +7,7 @@ import { SearchInput as UISearchInput } from '@faststore/ui'
 import { navigate } from 'gatsby'
 import type { ChangeEvent } from 'react'
 import { forwardRef, Suspense, useState, useRef } from 'react'
+import debounce from 'lodash.debounce'
 import useSearchHistory from 'src/sdk/search/useSearchHistory'
 import type { SearchEvent } from '@faststore/sdk'
 import type {
@@ -46,6 +47,7 @@ const SearchInput = forwardRef<SearchInputRef, SearchInputProps>(
   ) {
     const [searchDropdownOpen, setSearchDropdownOpen] = useState<boolean>(false)
     const [searchQuery, setSearchQuery] = useState<string>('')
+    const [deferredTerm, setDeferredTerm] = useState<string>('')
     const searchRef = useRef<HTMLDivElement>(null)
     const { addToSearchHistory } = useSearchHistory()
     const handleSearch = (term: string) => {
@@ -55,8 +57,13 @@ const SearchInput = forwardRef<SearchInputRef, SearchInputProps>(
 
     useOnClickOutside(searchRef, () => setSearchDropdownOpen(false))
 
+    const handleChangeDeferredTerm = useRef(
+      debounce((value: string) => setDeferredTerm(value), 500)
+    )
+
     const handleChangeTerm = (event: ChangeEvent<{ value: string }>) => {
       setSearchQuery(event.target.value)
+      handleChangeDeferredTerm.current(event.target.value)
     }
 
     return (
@@ -84,12 +91,13 @@ const SearchInput = forwardRef<SearchInputRef, SearchInputProps>(
           onSubmit={handleSearch}
           onFocus={() => setSearchDropdownOpen(true)}
           onChange={handleChangeTerm}
+          value={searchQuery}
           {...props}
         />
         {searchDropdownOpen && (
           <Suspense fallback={null}>
             <div data-store-search-input-dropdown-wrapper>
-              <SearchDropdown term={searchQuery} />
+              <SearchDropdown term={deferredTerm} />
             </div>
           </Suspense>
         )}
