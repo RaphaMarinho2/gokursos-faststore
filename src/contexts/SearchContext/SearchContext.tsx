892c621ecd107f47c4f87237311a145b28ebaac9
Rodrigo Muniz <rodrigo.muniz@acct.global>
2022-08-18 17:21:43 -0300

    feat(product-gallery): show searched products on the gallery

--- src/contexts/SearchContext/SearchContext.tsx
+++ src/contexts/SearchContext/SearchContext.tsx
@@ -1,12 +1,15 @@
+import type { SearchState } from '@faststore/sdk'
 import axios from 'axios'
 import type { Dispatch, SetStateAction } from 'react'
 import { createContext, useEffect, useState } from 'react'
 import type { ProductsProductCard } from 'src/components/product/ProductCard/ProductCard'
 import type { Filters } from 'src/components/search/PLPFilters/Filters'
+import { ITEMS_PER_PAGE } from 'src/constants'
 
 type SearchProviderProps = {
   slug: string
   children: React.ReactNode
+  searchParams?: SearchState
 }
 
 interface SearchContextProps {
@@ -23,7 +26,7 @@ export const SearchContext = createContext<SearchContextProps | undefined>(
   undefined
 )
 
-function SearchProvider({ children, slug }: SearchProviderProps) {
+function SearchProvider({ children, slug, searchParams }: SearchProviderProps) {
   const [filteredFacets, setFilteredFacets] = useState<Filters[]>([])
   const [productsCount, setProductsCount] = useState<number>(0)
 
@@ -37,22 +40,47 @@ function SearchProvider({ children, slug }: SearchProviderProps) {
       slug,
     }
 
-    const changeProducts = async () => {
-      const { value, '@odata.count': count } = await axios
-        .post(
-          '/api/getDepartmentOrCategory',
-          filteredFacets.length ? { ...body, filteredFacets } : body
-        )
-        .then(({ data }) => data)
-        .catch((err) => console.error(err))
+    setIsLoading(true)
 
-      setProductsCount(count)
-      setProducts(value)
-      setIsLoading(false)
+    if (slug) {
+      const changeProducts = async () => {
+        const { value, '@odata.count': count } = await axios
+          .post(
+            '/api/getDepartmentOrCategory',
+            filteredFacets.length ? { ...body, filteredFacets } : body
+          )
+          .then(({ data }) => data)
+          .catch((err) => console.error(err))
+          .finally(() => setIsLoading(false))
+
+        setProductsCount(count)
+        setProducts(value)
+      }
+
+      changeProducts()
     }
 
-    changeProducts()
-  }, [filteredFacets, slug])
+    if (searchParams) {
+      const changeProducts = async () => {
+        const { term, sort, page } = searchParams
+        const { value, '@odata.count': count } = await axios
+          .post('/api/getSearch', {
+            term,
+            sort,
+            skip: page * ITEMS_PER_PAGE,
+            itemsPerPage: ITEMS_PER_PAGE,
+          })
+          .then(({ data }) => data)
+          .catch((err) => console.error(err))
+          .finally(() => setIsLoading(false))
+
+        setProductsCount(count)
+        setProducts(value)
+      }
+
+      changeProducts()
+    }
+  }, [filteredFacets, searchParams, slug])
 
   const value = {
     isLoading,
