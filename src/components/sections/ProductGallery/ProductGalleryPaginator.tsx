7b16aad4a3f0b79bfa5c0c2fea3d5cfb6d19c1f2
Rodrigo Muniz <rodrigo.muniz@acct.global>
2022-08-23 12:01:09 -0300

    feat(product-gallery-paginator): implement add and back page behavior

--- src/components/sections/ProductGallery/ProductGalleryPaginator.tsx
+++ src/components/sections/ProductGallery/ProductGalleryPaginator.tsx
@@ -1,3 +1,4 @@
+import { navigate } from 'gatsby'
 import HorizontalArrowIcon from 'src/components/icons/HorizontalArrow'
 import Button from 'src/components/ui/Button'
 
@@ -11,18 +12,76 @@ interface ProductGalleryPaginatorProps {
 const ProductGalleryPaginator = ({
   currentPage,
   lastPage,
-}: ProductGalleryPaginatorProps) => (
-  <div className="product-listing__pagination product-listing__pagination--bottom">
-    <Button className="product-listing__pagination-button" variant="tertiary">
-      <HorizontalArrowIcon />
-    </Button>
-    <div className="product-listing__current-page">{currentPage}</div>
-    <span className="product-listing__page-separator">de</span>
-    <div className="product-listing__last-page">{lastPage}</div>
-    <Button className="product-listing__pagination-button" variant="tertiary">
-      <HorizontalArrowIcon direction="right" color="#FF3452" />
-    </Button>
-  </div>
-)
+}: ProductGalleryPaginatorProps) => {
+  const addPage = () => {
+    const { search, pathname } = window.location
+    const urlParams = new URLSearchParams(search)
+
+    if (!urlParams.has('page')) {
+      return
+    }
+
+    const current = parseInt(urlParams.get('page') ?? '0', 10)
+
+    urlParams.set('page', `${current + 1}`)
+
+    navigate(`${pathname}?${urlParams.toString()}`)
+  }
+
+  const backPage = () => {
+    const { search, pathname } = window.location
+    const urlParams = new URLSearchParams(search)
+
+    if (!urlParams.has('page')) {
+      return
+    }
+
+    const current = parseInt(urlParams.get('page') ?? '0', 10)
+
+    urlParams.set('page', `${current - 1}`)
+
+    navigate(`${pathname}?${urlParams.toString()}`)
+  }
+
+  const checkPage = (page: number) => {
+    const { search } = window.location
+    const urlParams = new URLSearchParams(search)
+
+    if (!urlParams.has('page')) {
+      return
+    }
+
+    const current = parseInt(urlParams.get('page') ?? '0', 10)
+
+    return current === page - 1
+  }
+
+  return (
+    <div className="product-listing__pagination product-listing__pagination--bottom">
+      <Button
+        className="product-listing__pagination-button"
+        variant="tertiary"
+        onClick={backPage}
+        disabled={checkPage(1)}
+      >
+        <HorizontalArrowIcon color={checkPage(1) ? undefined : '#FF3452'} />
+      </Button>
+      <div className="product-listing__current-page">{currentPage}</div>
+      <span className="product-listing__page-separator">de</span>
+      <div className="product-listing__last-page">{lastPage}</div>
+      <Button
+        className="product-listing__pagination-button"
+        variant="tertiary"
+        onClick={addPage}
+        disabled={checkPage(lastPage)}
+      >
+        <HorizontalArrowIcon
+          direction="right"
+          color={checkPage(lastPage) ? undefined : '#FF3452'}
+        />
+      </Button>
+    </div>
+  )
+}
 
 export default ProductGalleryPaginator
