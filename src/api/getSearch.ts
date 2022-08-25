0b30ed752d8a4956d9dc5203e0c6ee7b6fc43bb4
Rodrigo Muniz <rodrigo.muniz@acct.global>
2022-08-25 09:24:59 -0300

    refactor(getsearch): change search to retrieve LinkId instead of ProductURL

--- src/api/getSearch.ts
+++ src/api/getSearch.ts
@@ -13,7 +13,7 @@ async function getSearch(
     const { term, sort = 'Rank/Score desc', skip = 0, itemsPerPage } = req.body
 
     const data = await axios.get(
-      `${process.env.GATSBY_CATALOG_BASE_URL}/odata/Catalog/v1/Products?$expand=SKU, Department, Category, Price, Checkout, Especificacao, CommercialCondition, TradePolicy, Stock, Rank, Especificacao/CargaHoraria&$filter=IsActive eq true and (contains(Name, '${term}') or contains(Department/Name, '${term}') or contains(Category/Name, '${term}') or contains(KeyWords, '${term}') or contains(Brand/Name, '${term}'))&$top=${itemsPerPage}&$skip=${skip}&$select=ID, Name, ProductImageURL, Price/BasePrice, Price/ListPrice, Price/CommisionedPrice, Price/isSale, Category/Name, Category/Slug, Especificacao/CargaHoraria/Text, ProductURL&$orderby=${sort}&$count=true`
+      `${process.env.GATSBY_CATALOG_BASE_URL}/odata/Catalog/v1/Products?$expand=SKU, Department, Category, Price, Checkout, Especificacao, CommercialCondition, TradePolicy, Stock, Rank, Especificacao/CargaHoraria&$filter=IsActive eq true and (contains(Name, '${term}') or contains(Department/Name, '${term}') or contains(Category/Name, '${term}') or contains(KeyWords, '${term}') or contains(Brand/Name, '${term}'))&$top=${itemsPerPage}&$skip=${skip}&$select=ID, Name, ProductImageURL, Price/BasePrice, Price/ListPrice, Price/CommisionedPrice, Price/isSale, Category/Name, Category/Slug, Especificacao/CargaHoraria/Text, LinkId&$orderby=${sort}&$count=true`
     )
 
     res.json(data.data ?? {})
