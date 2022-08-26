7ab76f083ed465695b1b5e13e5ff591dbbb8f15c
Rodrigo Muniz <rodrigo.muniz@acct.global>
2022-08-26 17:01:32 -0300

    fix(search-suggestions): search for term with utf8 format

--- src/api/getSuggestions.ts
+++ src/api/getSuggestions.ts
@@ -8,13 +8,20 @@ async function getSuggestions(
   try {
     const { term } = req.query
 
+    const encodedTerm = encodeURI(term)
+    const expand =
+      'SKU, Department, Category, Price, Checkout, Especificacao, CommercialCondition, TradePolicy, Stock, Rank, Especificacao/CargaHoraria'
+
+    const filter = `IsActive eq true and (contains(Name, '${encodedTerm}') or contains(Department/Name, '${encodedTerm}') or contains(Category/Name, '${encodedTerm}') or contains(KeyWords, '${encodedTerm}') or contains(Brand/Name, '${encodedTerm}'))`
+
     const data = await axios.get(
-      `${process.env.GATSBY_CATALOG_BASE_URL}/odata/Catalog/v1/Products?$expand=SKU, Department, Category, Price, Checkout, Especificacao, CommercialCondition, TradePolicy, Stock, Rank, Especificacao/CargaHoraria&$filter=IsActive eq true and (contains(Name, '${term}') or contains(Department/Name, '${term}') or contains(Category/Name, '${term}') or contains(KeyWords, '${term}') or contains(Brand/Name, '${term}'))&$top=8&$skip=0&$select=Name, ProductImageURL, LinkId`
+      `${process.env.GATSBY_CATALOG_BASE_URL}/odata/Catalog/v1/Products?$expand=${expand}&$filter=${filter}&$top=8&$skip=0&$select=Name, ProductImageURL, LinkId`
     )
 
     res.json(data.data?.value ?? [])
   } catch (error) {
-    throw new Error(error)
+    res.status(error.response.status)
+    res.json([])
   }
 }
 
