2addb512fb8643c530dab4132ff30f978d83562f
Rodrigo Muniz <rodrigo.muniz@acct.global>
2022-08-31 12:10:14 -0300

    feat(my-account): create my account pages structure

---
+++ src/pages/minha-conta/[...].tsx
@@ -0,0 +1,18 @@
+import { Router } from '@reach/router'
+import MyAccountMenu from 'src/components/sections/MyAccountMenu'
+import MyOrders from 'src/components/sections/MyOrders'
+import Section from 'src/components/sections/Section'
+import WithPrivateRoute from 'src/components/sections/WithPrivateRoute/WithPrivateRoute'
+
+const PrivateMyOrders = WithPrivateRoute(MyOrders)
+
+export default function MyAccount() {
+  return (
+    <Section className="layout__content">
+      <MyAccountMenu />
+      <Router basepath="/minha-conta" className="layout__content-full">
+        <PrivateMyOrders path="/pedidos" />
+      </Router>
+    </Section>
+  )
+}
