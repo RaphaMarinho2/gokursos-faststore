9f0b0442ecb7aa8c466984cf639d1c37f2c40724
Rodrigo Muniz <rodrigo.muniz@acct.global>
2022-08-31 17:03:06 -0300

    feat(my-account-menu): add mobile menu

--- src/components/sections/MyAccountMenu/MyAccountMenu.tsx
+++ src/components/sections/MyAccountMenu/MyAccountMenu.tsx
@@ -1,8 +1,48 @@
 import { Link } from 'gatsby'
+import { useRef, useState } from 'react'
+import DropDownIcon from 'src/components/icons/DropDownIcon'
+import useOnClickOutside from 'src/hooks/useOnClickOutside'
+import useWindowDimensions from 'src/sdk/utils/useWindowDimensions'
 
 import './my-account-menu.scss'
 
 export default function MyAccountMenu() {
+  const [dropdowmOpen, setDropdownOpen] = useState<boolean>(false)
+  const mobileMenuRef = useRef<HTMLDivElement>(null)
+  const { isTablet } = useWindowDimensions()
+
+  const showDropdown = () => setDropdownOpen(!dropdowmOpen)
+
+  useOnClickOutside(mobileMenuRef, () => setDropdownOpen(false))
+
+  if (isTablet) {
+    return (
+      <div className="my-account-mobile-menu" ref={mobileMenuRef}>
+        <button
+          className="my-account-mobile-menu__selected"
+          onClick={showDropdown}
+        >
+          Pedidos{' '}
+          <div className="dropdown-icon">
+            <DropDownIcon />
+          </div>
+        </button>
+        {dropdowmOpen && (
+          <ul className="my-account-mobile-menu__links">
+            <li className="my-account-mobile-menu__links-item">
+              <Link className="link" to="/minha-conta/pedidos">
+                Pedidos
+              </Link>
+            </li>
+            <li className="my-account-mobile-menu__links-item">
+              <button className="logout">Sair</button>
+            </li>
+          </ul>
+        )}
+      </div>
+    )
+  }
+
   return (
     <div className="my-account-menu">
       <div className="my-account-menu__greetings">
