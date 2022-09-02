20ad361f98a35ccb135d83eeac1e1e6e54627e0d
Rodrigo Muniz <rodrigo.muniz@acct.global>
2022-09-02 17:10:16 -0300

    feat(my-orders): show user orders on the orders page

--- src/components/sections/MyOrders/MyOrders.tsx
+++ src/components/sections/MyOrders/MyOrders.tsx
@@ -1,15 +1,46 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 import './my-orders.scss'
+import Order from './Order'
+import type { OrderData } from './typings'
 
 export default function MyOrders() {
+  const [orderData, setOrderData] = useState<OrderData[] | []>([])
+
+  useEffect(() => {
+    const userData =
+      typeof window !== 'undefined' ? window.localStorage.getItem('user') : ''
+
+    axios
+      .post('/api/getOrders', userData ? JSON.parse(userData) : null)
+      .then((resp) => setOrderData(resp.data))
+      .catch((error) => console.error(error))
+  }, [])
+
   return (
     <div className="my-orders">
       <h3 className="my-orders__title">Pedidos</h3>
-      <div className="my-orders__content">
-        <span className="empty-title">Opss!!!</span>
-        <span className="empty-text">
-          Você ainda não possui pedidos abertos!
-        </span>
-      </div>
+      {orderData?.length ? (
+        <div className="my-orders__content">
+          <ul className="my-orders__list">
+            {orderData.map((order: OrderData) => {
+              if (!order.Pedidos) {
+                return <></>
+              }
+
+              return <Order orderData={order} key={order.Pedidos[0].OrderID} />
+            })}
+          </ul>
+        </div>
+      ) : (
+        <div className="my-orders__empty-content">
+          <span className="empty-title">Opss!!!</span>
+          <span className="empty-text">
+            Você ainda não possui pedidos abertos!
+          </span>
+        </div>
+      )}
     </div>
   )
 }
---
+++ src/components/sections/MyOrders/Order.tsx
@@ -0,0 +1,31 @@
+import { useFormattedPrice } from 'src/sdk/product/useFormattedPrice'
+
+import type { OrderData } from './typings'
+import './my-orders.scss'
+
+interface OrdersProps {
+  orderData: OrderData
+}
+
+export default function Order({ orderData }: OrdersProps) {
+  const orderValue = useFormattedPrice(orderData.Pedidos[0].ValorTransacionado)
+  const orderDate = new Date(orderData.Pedidos[0].DataHora)
+
+  return (
+    <li className="my-orders__list-item">
+      <div className="my-orders__list-item-info">
+        <span className="title">{`Pedido #${orderData.Pedidos[0].OrderID}`}</span>
+        <span className="order-date">{`${orderDate.toLocaleDateString(
+          'pt-Br'
+        )} às ${orderDate.toLocaleTimeString('pt-Br')}`}</span>
+        <span className="order-value">{orderValue}</span>
+        <span className="order-status">
+          Status: {orderData.StatusDescription}
+        </span>
+      </div>
+      <button className="my-orders__list-item-button">
+        Ver detalhes do pedido
+      </button>
+    </li>
+  )
+}
