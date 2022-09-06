32f09ca17f77a7304e91374922cab865e5e2379a
Rodrigo Muniz <rodrigo.muniz@acct.global>
2022-09-06 17:17:32 -0300

    feat(my-orders): my account logout modal

---
+++ src/components/icons/RedWarning.tsx
@@ -0,0 +1,32 @@
+const RedWarningIcon = () => (
+  <svg
+    width="73"
+    height="73"
+    viewBox="0 0 73 73"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M36.4998 7.604L3.0415 65.3957H69.9582L36.4998 7.604Z"
+      stroke="#FF3452"
+      strokeWidth="3"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M36.5 53.229V54.7498"
+      stroke="#FF3452"
+      strokeWidth="3"
+      strokeLinecap="round"
+    />
+    <path
+      d="M36.5 28.8965L36.5126 44.1041"
+      stroke="#FF3452"
+      strokeWidth="3"
+      strokeLinecap="round"
+    />
+  </svg>
+)
+
+export default RedWarningIcon
--- src/components/sections/MyAccountMenu/MyAccountMenu.tsx
+++ src/components/sections/MyAccountMenu/MyAccountMenu.tsx
@@ -1,13 +1,37 @@
-import { Link } from 'gatsby'
+import { Link, navigate } from 'gatsby'
 import { useRef, useState } from 'react'
+import Modal from 'react-modal'
 import DropDownIcon from 'src/components/icons/DropDownIcon'
+import RedWarningIcon from 'src/components/icons/RedWarning'
 import useOnClickOutside from 'src/hooks/useOnClickOutside'
 import useWindowDimensions from 'src/sdk/utils/useWindowDimensions'
 
 import './my-account-menu.scss'
 
+const CUSTOM_STYLES = {
+  overlay: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: 999,
+    paddingLeft: 16,
+    paddingRight: 16,
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+  },
+  content: {
+    height: 'fit-content',
+    position: undefined,
+    borderRadius: 10,
+  },
+}
+
+interface LogoutModalContentProps {
+  setModalIsOpen: (modalIsOpen: boolean) => void
+}
+
 export default function MyAccountMenu() {
   const [dropdowmOpen, setDropdownOpen] = useState<boolean>(false)
+  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false)
   const mobileMenuRef = useRef<HTMLDivElement>(null)
   const { isTablet } = useWindowDimensions()
 
@@ -15,46 +39,94 @@ export default function MyAccountMenu() {
 
   useOnClickOutside(mobileMenuRef, () => setDropdownOpen(false))
 
+  const openModal = () => setModalIsOpen(true)
+
   if (isTablet) {
     return (
-      <div className="my-account-mobile-menu" ref={mobileMenuRef}>
-        <button
-          className="my-account-mobile-menu__selected"
-          onClick={showDropdown}
-        >
-          Pedidos{' '}
-          <div className="dropdown-icon">
-            <DropDownIcon />
-          </div>
-        </button>
-        {dropdowmOpen && (
-          <ul className="my-account-mobile-menu__links">
-            <li className="my-account-mobile-menu__links-item">
-              <Link className="link" to="/minha-conta/pedidos">
-                Pedidos
-              </Link>
-            </li>
-            <li className="my-account-mobile-menu__links-item">
-              <button className="logout">Sair</button>
-            </li>
-          </ul>
-        )}
-      </div>
+      <>
+        <div className="my-account-mobile-menu" ref={mobileMenuRef}>
+          <button
+            className="my-account-mobile-menu__selected"
+            onClick={showDropdown}
+          >
+            Pedidos{' '}
+            <div className="dropdown-icon">
+              <DropDownIcon />
+            </div>
+          </button>
+          {dropdowmOpen && (
+            <ul className="my-account-mobile-menu__links">
+              <li className="my-account-mobile-menu__links-item">
+                <Link className="link" to="/minha-conta/pedidos">
+                  Pedidos
+                </Link>
+              </li>
+              <li className="my-account-mobile-menu__links-item">
+                <button className="logout" onClick={openModal}>
+                  Sair
+                </button>
+              </li>
+            </ul>
+          )}
+        </div>
+        <Modal isOpen={modalIsOpen} style={CUSTOM_STYLES}>
+          <LogoutModalContent setModalIsOpen={setModalIsOpen} />
+        </Modal>
+      </>
     )
   }
 
   return (
-    <div className="my-account-menu">
-      <div className="my-account-menu__greetings">
-        <span className="my-account-menu__greetings-text">Olá,</span>
-        <span className="my-account-menu__greetings-name">Thales!</span>
+    <>
+      <div className="my-account-menu">
+        <div className="my-account-menu__greetings">
+          <span className="my-account-menu__greetings-text">Olá,</span>
+          <span className="my-account-menu__greetings-name">Thales!</span>
+        </div>
+        <nav className="my-account-menu__links">
+          <Link
+            className="my-account-menu__links-item"
+            to="/minha-conta/pedidos"
+          >
+            Pedidos
+          </Link>
+          <button className="my-account-menu__links-logout" onClick={openModal}>
+            Sair
+          </button>
+        </nav>
       </div>
-      <nav className="my-account-menu__links">
-        <Link className="my-account-menu__links-item" to="/minha-conta/pedidos">
-          Pedidos
-        </Link>
-        <button className="my-account-menu__links-logout">Sair</button>
-      </nav>
+      <Modal isOpen={modalIsOpen} style={CUSTOM_STYLES}>
+        <LogoutModalContent setModalIsOpen={setModalIsOpen} />
+      </Modal>
+    </>
+  )
+}
+
+function LogoutModalContent({ setModalIsOpen }: LogoutModalContentProps) {
+  const closeModal = () => setModalIsOpen(false)
+
+  const logout = () => {
+    navigate('/')
+    if (typeof window !== 'undefined') {
+      window.localStorage.removeItem('user')
+    }
+  }
+
+  return (
+    <div className="logout-modal">
+      <RedWarningIcon />
+      <span className="title">Encerrar sessão</span>
+      <span className="text">
+        Tem certeza que deseja encerrar a sessão da sua conta?
+      </span>
+      <div className="buttons-container">
+        <button className="exit-button" onClick={logout}>
+          Sair
+        </button>
+        <button className="cancel-button" onClick={closeModal}>
+          Cancelar
+        </button>
+      </div>
     </div>
   )
 }
