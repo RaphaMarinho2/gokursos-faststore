a07108f5cd22d0a4e82f9d307ba8631e8d52c489
Rodrigo Muniz <rodrigo.muniz@acct.global>
2022-09-06 14:04:06 -0300

    refactor(order-preferences): create a component to order preferences

---
+++ src/components/sections/MyOrders/OrdersPreferences.tsx
@@ -0,0 +1,21 @@
+import type { OrderData } from './typings'
+
+import './my-orders.scss'
+
+interface OrderPreferencesProps {
+  orderDetails: OrderData
+}
+
+export default function OrderPreferences({
+  orderDetails,
+}: OrderPreferencesProps) {
+  return (
+    <div className="my-order__details-preferences">
+      <h4 className="title">Preferências</h4>
+      <span className="subtitle">Método de pagamento</span>
+      <span className="text">
+        {orderDetails.Pedidos[0].PaymentMethodCore.Name}
+      </span>
+    </div>
+  )
+}
