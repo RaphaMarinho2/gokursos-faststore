2a50adbd99bea09abab14ed7221ba5bfacbf994a
Willy de Oliveira <111988577+WillyOliveiraS@users.noreply.github.com>
2022-09-13 13:26:27 -0300

    Bugfix/gm 751 (#157)
    * feat(productshelf): added ProductShelfSkeleton component
    
    * refactor(productshelf): changed the Carousel dependency
    
    * fix(productshelf): fixed ProductCard layout
    
    * refactor(productshelf): changed isLoading value
    
    * docs(changelog): add changelog project

--- src/components/sections/HomeProductShelf/HomeProductShelf.tsx
+++ src/components/sections/HomeProductShelf/HomeProductShelf.tsx
@@ -25,12 +25,14 @@ const HomeProductShelf = ({ title, pretitle }: ShelfProps) => {
 
   return (
     <Section className="layout__content home-shelf-container">
-      <ProductShelf
-        cardsQuantity={shelfItemQuantity}
-        title={title}
-        pretitle={pretitle}
-        products={products}
-      />
+      {products && (
+        <ProductShelf
+          cardsQuantity={shelfItemQuantity}
+          title={title}
+          pretitle={pretitle}
+          products={products}
+        />
+      )}
     </Section>
   )
 }
