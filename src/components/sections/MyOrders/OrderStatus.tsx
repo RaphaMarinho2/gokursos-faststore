ad3aa1cef2360ad8a76bc0f1c96d3aacb8c5be6f
Rodrigo Muniz <rodrigo.muniz@acct.global>
2022-09-19 10:03:20 -0300

    feat(order-status): change order status to be a simple text

--- src/components/sections/MyOrders/OrderStatus.tsx
+++ src/components/sections/MyOrders/OrderStatus.tsx
@@ -1,6 +1,3 @@
-import WarningIcon from 'src/components/icons/Warning'
-import ConfirmedPaymentIcon from 'src/components/icons/ConfirmedPayment'
-
 import type { OrderData } from './typings'
 import './my-orders.scss'
 
@@ -9,25 +6,7 @@ interface OrderStatusProps {
 }
 
 export default function OrderStatus({ orderData }: OrderStatusProps) {
-  const { Status: status, StatusDescription: statusDescription } = orderData
-
-  if (status === 'payment-pending') {
-    return (
-      <>
-        <span className="order-status__pending">{statusDescription}</span>
-        <WarningIcon />
-      </>
-    )
-  }
-
-  if (status === 'ready-for-handling') {
-    return (
-      <>
-        <span className="order-status__approved">Pagamento Aprovado</span>
-        <ConfirmedPaymentIcon />
-      </>
-    )
-  }
+  const { StatusDescription: statusDescription } = orderData
 
   return <span className="order-status__text">{statusDescription}</span>
 }
