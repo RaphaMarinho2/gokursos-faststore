e0e70786572485138d65372c23a8211862205536
Raphael Marinho <raphael.marinho@acct.global>
2022-09-19 14:27:19 -0300

    fix: server variable test

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -4,19 +4,62 @@ import type { GatsbyNode } from 'gatsby'
 
 import { apiSchema } from './src/server'
 
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
-
 // Log out information after a build is done
 exports.onPostBuild = ({ reporter }: any) => {
   reporter.info(`Your Gatsby site has been built!`)
 }
 
+// Create department pages dynamically
+exports.createPages = async ({ graphql, actions }: any) => {
+  const { createPage } = actions
+  const departmentPage = path.resolve(`src/pages/[...slug].tsx`)
+  const result = await graphql(`
+    query {
+      allContentfulPageDepartmentCategory {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allContentfulPageDepartmentCategory.edges.forEach((edge: any) => {
+    createPage({
+      path: `${edge.node.slug}`,
+      component: departmentPage,
+      context: {
+        slug: edge.node.slug,
+      },
+    })
+  })
+
+  const baseUrl =
+    process.env.NODE_ENV === 'development'
+      ? process.env.GATSBY_CATALOG_BASE_URL
+      : process.env.SERVER_CATALOG_BASE_URL
+
+  // Create product pages dynamically
+  const productPage = path.resolve(`src/pages/[slug]/p.tsx`)
+  const resultPDP = await fetch(
+    `${baseUrl}/odata/Catalog/v1/Products?$select=LinkId`
+  ).then((response) => response.json())
+
+  resultPDP.value.forEach((edge: any) => {
+    createPage({
+      path: `${edge.LinkId}/p`,
+      component: productPage,
+      context: {
+        slug: edge.LinkId,
+      },
+    })
+  })
+}
+
 export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({
   actions: { setWebpackConfig },
   stage,
-  plugins,
 }) => {
   const isProfilingEnabled = process.env.GATSBY_STORE_PROFILING === 'true'
 
@@ -32,11 +75,6 @@ export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({
       })
     } else {
       setWebpackConfig({
-        plugins: [
-          plugins.define({
-            GATSBY_CATALOG_BASE_URL: process.env.GATSBY_CATALOG_BASE_URL,
-          }),
-        ],
         optimization: {
           runtimeChunk: {
             name: `webpack-runtime`,
@@ -75,46 +113,3 @@ export const createSchemaCustomization: GatsbyNode['createSchemaCustomization'] 
 
     actions.addThirdPartySchema({ schema })
   }
-
-// Create department pages dynamically
-exports.createPages = async ({ graphql, actions }: any) => {
-  const { createPage } = actions
-  const departmentPage = path.resolve(`src/pages/[...slug].tsx`)
-  const result = await graphql(`
-    query {
-      allContentfulPageDepartmentCategory {
-        edges {
-          node {
-            slug
-          }
-        }
-      }
-    }
-  `)
-
-  result.data.allContentfulPageDepartmentCategory.edges.forEach((edge: any) => {
-    createPage({
-      path: `${edge.node.slug}`,
-      component: departmentPage,
-      context: {
-        slug: edge.node.slug,
-      },
-    })
-  })
-
-  // Create product pages dynamically
-  const productPage = path.resolve(`src/pages/[slug]/p.tsx`)
-  const resultPDP = await fetch(
-    `${process.env.GATSBY_CATALOG_BASE_URL}/odata/Catalog/v1/Products?$select=LinkId`
-  ).then((response) => response.json())
-
-  resultPDP.value.forEach((edge: any) => {
-    createPage({
-      path: `${edge.LinkId}/p`,
-      component: productPage,
-      context: {
-        slug: edge.LinkId,
-      },
-    })
-  })
-}
