82b624624fe2fd551dc3455f4d8747155ae03f8d
Wellington Costa <wellington.costa@acct.global>
2022-09-19 19:34:45 -0300

    fix(plansshelf): adjust selector of plans

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed 
+
+- **GM-783** - [Planos] Adjust select and options
+
 ## [0.17.1] - 2022-09-15
 
 ### Fixed 
--- src/components/sections/PlansShelf/PlansShelf.tsx
+++ src/components/sections/PlansShelf/PlansShelf.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from 'react'
 import axios from 'axios'
-import useWindowDimensions from 'src/sdk/utils/useWindowDimensions'
+import { useEffect, useState } from 'react'
 import ProductShelf from 'src/components/product/ProductShelf'
-
+import useWindowDimensions from 'src/sdk/utils/useWindowDimensions'
+import MenuItem from '@mui/material/MenuItem'
+import type { SelectChangeEvent } from '@mui/material/Select'
+import Select from '@mui/material/Select'
 import './plans-shelf.scss'
 
 const plansMock = [
@@ -43,30 +45,40 @@ function PlansShelf() {
 
   const shelfItemQuantity = isMobile ? 2 : isTablet ? 4 : 5
 
+  const [plans, setPlans] = useState(plansMock[0].name)
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setPlans(event.target.value as string)
+  }
+
   const ShelfTitle = () => {
     return (
       <h2 className="plans-shelf__title">
         Cursos que você encontra no{' '}
-        <select name="plans" id="plans">
-          {plansMock.map((plan) => (
-            <option key={plan.value} value={plan.value}>
+        <Select
+          labelId="select-label"
+          id="simple-select"
+          onChange={handleChange}
+          value={plans}
+        >
+          {plansMock.map((plan, index) => (
+            <MenuItem key={index} value={plan.name}>
               {plan.name}
-            </option>
+            </MenuItem>
           ))}
-        </select>
+        </Select>
       </h2>
     )
   }
 
   return (
     <div className="plans-shelf">
-      {products && (
-        <ProductShelf
-          cardsQuantity={shelfItemQuantity}
-          title={<ShelfTitle />}
-          products={products}
-        />
-      )}
+      <ProductShelf
+        cardsQuantity={shelfItemQuantity}
+        title={<ShelfTitle />}
+        products={products}
+        isLoading={products === undefined}
+      />
     </div>
   )
 }
