093cb93e7af0b4a14caa8a1fc8a0d586ca583898
FerrazLf <ferraz.lf00@gmail.com>
2022-09-23 12:29:28 -0300

    fix(commonquestion): refactor component Common Question

--- src/components/sections/CommonQuestions/CommonQuestions.tsx
+++ src/components/sections/CommonQuestions/CommonQuestions.tsx
@@ -12,42 +12,17 @@ type CommonQuestions = {
   }>
 }
 
-const columnDivider = (nodes: CommonQuestions['nodes']) => {
-  const middleIndex = Math.ceil(nodes.length / 2)
-
-  const firstColumn = nodes.splice(0, middleIndex)
-  const secondColumn = nodes.splice(-middleIndex)
-
-  return {
-    firstColumn,
-    secondColumn,
-  }
-}
-
 const CommonQuestions = ({ nodes }: CommonQuestions) => {
   if (!nodes || nodes.length < 1) {
     return null
   }
-
-  const { firstColumn, secondColumn } = columnDivider(nodes)
 
   return (
     <div className="common-questions">
       <h2 className="common-questions__title">Principais Perguntas</h2>
       <div className="common-questions__container">
         <div className="common-questions__row">
-          {firstColumn.map((content, idx) => (
-            <Accordion
-              key={idx}
-              title={content.question ?? ''}
-              content={content.answer?.answer ?? ''}
-              iconDefault={<Plus size={20} color="#004E98" />}
-              iconClicked={<Minus size={20} color="#004E98" />}
-            />
-          ))}
-        </div>
-        <div className="common-questions__row">
-          {secondColumn.map((content, idx) => (
+          {nodes.map((content, idx) => (
             <Accordion
               key={idx}
               title={content.question ?? ''}
