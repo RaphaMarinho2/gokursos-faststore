761dc5cbdddb5a154e9d680e783944455b2b95bb
Guilherme Macedo <guilherme.macedo@acct.global>
2022-10-03 20:49:01 -0300

    fix(api): adjust in API

--- src/api/getNewReleases.ts
+++ src/api/getNewReleases.ts
@@ -5,9 +5,15 @@ export default async function getNewReleases(
   _: GatsbyFunctionRequest,
   res: GatsbyFunctionResponse
 ) {
+  const expand = `SKU, Category, Price, Checkout, Especificacao, CommercialCondition, TradePolicy, Stock, Rank, Especificacao/CargaHoraria`
+
+  const filter = `IsActive eq true and IsVisible eq true`
+
+  const select = `Name, ProductImageURL,  Price/BasePrice, Price/ListPrice, Price/CommisionedPrice, Price/isSale, Category/Name, LinkId`
+
   try {
     const { data } = await axios.get(
-      `${process.env.GATSBY_CATALOG_BASE_URL}/odata/Catalog/v1/Products?$expand=SKU, Category, Price, Checkout, Especificacao, CommercialCondition, TradePolicy, Stock, Rank, Especificacao/CargaHoraria&$orderby=ReleaseDate desc&$filter=IsActive eq true and IsVisible eq true&$top=20&$skip=0&$select=Name, ProductImageURL,  Price/BasePrice, Price/ListPrice, Price/CommisionedPrice, Price/isSale, Category/Name, LinkId`
+      `${process.env.GATSBY_CATALOG_BASE_URL}/odata/Catalog/v1/Products?$expand=${expand}&$orderby=ReleaseDate desc&$filter=${filter}&$top=20&$skip=0&$select=${select}`
     )
 
     res.json(data)
