07b238edcdbdeb6fe682e8e30787b0785972ea3d
Guilherme Macedo <guilherme.macedo@acct.global>
2022-10-04 09:43:53 -0300

    feat(api): adjust in api for project

--- src/api/getNewReleases.ts
+++ src/api/getNewReleases.ts
@@ -7,13 +7,17 @@ export default async function getNewReleases(
 ) {
   const expand = `SKU, Category, Price, Checkout, Especificacao, CommercialCondition, TradePolicy, Stock, Rank, Especificacao/CargaHoraria`
 
+  const orderBy = `ReleaseDate desc`
+
   const filter = `IsActive eq true and IsVisible eq true`
 
   const select = `Name, ProductImageURL,  Price/BasePrice, Price/ListPrice, Price/CommisionedPrice, Price/isSale, Category/Name, LinkId`
 
+  const top = `20`
+
   try {
     const { data } = await axios.get(
-      `${process.env.GATSBY_CATALOG_BASE_URL}/odata/Catalog/v1/Products?$expand=${expand}&$orderby=ReleaseDate desc&$filter=${filter}&$top=20&$skip=0&$select=${select}`
+      `${process.env.GATSBY_CATALOG_BASE_URL}/odata/Catalog/v1/Products?$expand=${expand}&$orderby=${orderBy}&$filter=${filter}&$top=${top}&$select=${select}`
     )
 
     res.json(data)
