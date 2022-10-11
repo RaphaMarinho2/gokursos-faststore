73b5a7983bd585c35b82c3d46a575e38618302c2
Raphael Marinho <raphael.marinho@acct.global>
2022-10-11 11:16:45 -0300

    fix: section test

--- src/components/sections/PlansShelf/PlansShelf.tsx
+++ src/components/sections/PlansShelf/PlansShelf.tsx
@@ -2,37 +2,39 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 import ProductShelf from 'src/components/product/ProductShelf'
 import useWindowDimensions from 'src/sdk/utils/useWindowDimensions'
-// import MenuItem from '@mui/material/MenuItem'
-// import type { SelectChangeEvent } from '@mui/material/Select'
-// import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+import type { SelectChangeEvent } from '@mui/material/Select'
+import Select from '@mui/material/Select'
+
+import Section from '../Section'
 import './plans-shelf.scss'
 
-// const plansMock = [
-//   {
-//     name: 'Plano Básico',
-//     value: 'basico',
-//   },
-//   {
-//     name: 'Plano Especial',
-//     value: 'especial',
-//   },
-//   {
-//     name: 'Plano Comunicação',
-//     value: 'comunicacao',
-//   },
-//   {
-//     name: 'Plano Tecnologia',
-//     value: 'tecnologia',
-//   },
-//   {
-//     name: 'Plano Saúde',
-//     value: 'saude',
-//   },
-//   {
-//     name: 'Plano Negócios',
-//     value: 'negocios',
-//   },
-// ]
+const plansMock = [
+  {
+    name: 'Plano Básico',
+    value: 'basico',
+  },
+  {
+    name: 'Plano Especial',
+    value: 'especial',
+  },
+  {
+    name: 'Plano Comunicação',
+    value: 'comunicacao',
+  },
+  {
+    name: 'Plano Tecnologia',
+    value: 'tecnologia',
+  },
+  {
+    name: 'Plano Saúde',
+    value: 'saude',
+  },
+  {
+    name: 'Plano Negócios',
+    value: 'negocios',
+  },
+]
 
 function PlansShelf() {
   const [products, setProducts] = useState<any>()
@@ -49,41 +51,41 @@ function PlansShelf() {
 
   const shelfItemQuantity = isMobile ? 2 : isTablet ? 4 : 5
 
-  // const [plans, setPlans] = useState(plansMock[0].name)
+  const [plans, setPlans] = useState(plansMock[0].name)
 
-  // const handleChange = (event: SelectChangeEvent) => {
-  //   setPlans(event.target.value as string)
-  // }
+  const handleChange = (event: SelectChangeEvent) => {
+    setPlans(event.target.value as string)
+  }
 
-  // const ShelfTitle = () => {
-  //   return (
-  //     <h2 className="plans-shelf__title">
-  //       Cursos que você encontra no{' '}
-  //       <Select
-  //         labelId="select-label"
-  //         id="simple-select"
-  //         onChange={handleChange}
-  //         value={plans}
-  //       >
-  //         {plansMock.map((plan, index) => (
-  //           <MenuItem key={index} value={plan.name}>
-  //             {plan.name}
-  //           </MenuItem>
-  //         ))}
-  //       </Select>
-  //     </h2>
-  //   )
-  // }
+  const ShelfTitle = () => {
+    return (
+      <h2 className="plans-shelf__title">
+        Cursos que você encontra no{' '}
+        <Select
+          labelId="select-label"
+          id="simple-select"
+          onChange={handleChange}
+          value={plans}
+        >
+          {plansMock.map((plan, index) => (
+            <MenuItem key={index} value={plan.name}>
+              {plan.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </h2>
+    )
+  }
 
   return (
-    <div className="plans-shelf">
+    <Section className="layout__content plans-shelf">
       <ProductShelf
         cardsQuantity={shelfItemQuantity}
-        // title={<ShelfTitle />}
+        title={<ShelfTitle />}
         products={products}
         isLoading={isLoading}
       />
-    </div>
+    </Section>
   )
 }
 
