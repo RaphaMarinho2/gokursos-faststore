e2cb98d9324a520ada6c4ad8508eb4eba9401f5b
Guilherme Aires <guilherme.macedo@acct.global>
2022-10-13 12:55:36 -0300

    fix(buybox): adjust in the name of div

--- src/components/sections/BuyBox/BuyBox.tsx
+++ src/components/sections/BuyBox/BuyBox.tsx
@@ -70,7 +70,7 @@ const BuyBox = ({ nodes, href }: BuyBoxProps) => {
               alt={titulo}
               src={isTablet ? bannerImageMobile?.url : bannerImageDesktop?.url}
             />
-            <div className="test">
+            <div className="buy-box__section">
               <div key={index} className="component-Buybox__content">
                 <div className="titulo-compartilhar">
                   <h2 className="titulo-planos">{titulo}</h2>
