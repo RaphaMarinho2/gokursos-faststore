b1800fcf342a809515015bba329f4126588c9157
Rodrigo Muniz <rodrigo.muniz@acct.global>
2022-10-27 11:05:31 -0300

    fix(my-courses): add section component to improve responsivity for ultra wide layouts

--- src/pages/meus-cursos/index.tsx
+++ src/pages/meus-cursos/index.tsx
@@ -1,6 +1,7 @@
 import { BreadcrumbJsonLd } from 'gatsby-plugin-next-seo'
 import Breadcrumb from 'src/components/sections/Breadcrumb'
 import MyCourses from 'src/components/sections/MyCourses'
+import Section from 'src/components/sections/Section'
 import { mark } from 'src/sdk/tests/mark'
 
 const BREADCRUMB = [
@@ -13,11 +14,11 @@ const BREADCRUMB = [
 
 function Page() {
   return (
-    <>
+    <Section>
       <BreadcrumbJsonLd itemListElements={BREADCRUMB} />
       <Breadcrumb breadcrumbList={BREADCRUMB} name="Meus Cursos" />
       <MyCourses />
-    </>
+    </Section>
   )
 }
 
